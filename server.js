e294bd238118721ddc23d7ecc91583f63567ce16
thanneman <thanneman1@gmail.com>
2020-01-07 10:47:01 -0700

    /types endpoint working

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-
+console.log(process.env.API_TOKEN);
 const app = express();
 
 app.use(morgan('dev'));
 
-app.use((req, res) => {
-    res.send('Hello World!');
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN;
+    const authToken = req.get('Authorization');
+
+    if (!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({ error: 'Unauthorized request' });
+    };
+    // move to the next middleware
+    next()
 });
+
+const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`];
+
+function handleGetTypes(req, res) {
+    res.json(validTypes);
+};
+
+app.get('/types', handleGetTypes);
+
+function handleGetPokemon(req, res) {
+    res.send('Hello Pokemon!');
+};
+
+app.get('/pokemon', handleGetPokemon);
 
 const PORT = 8000;
 
